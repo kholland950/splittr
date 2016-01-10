9fe8591bbc08a3ed8ea3673ceb8ad502b4d11a1f
Quinton Woodmansee <qwoodmansee@capital.edu>
2016-01-10 11:52:44 -0500

    Implemented Time Based updating of velocity and acceleration.
    Started using FlooBits to edit code simultaneously.

--- src/game.js
+++ src/game.js
@@ -3,8 +3,8 @@
  */
 
 keys = {};
-playerAccel = .4;
-playerDecel = .1;
+playerAccel = 1000; //.4
+playerDecel = 300;
 
 function init() {
     //Create a stage by getting a reference to the canvas
@@ -12,14 +12,19 @@ function init() {
 
     //create a square object
     playerSquare = new createjs.Shape();
+
+    //TODO(quinton): Fix start location magic numbers
     playerSquare.graphics.beginFill("#1643A3").drawRect(220,585,90,90);
+
     playerSquare.velocity = 0;
+
     stage.addChild(playerSquare);
     stage.update();
 
     //Update stage will render next frame
     createjs.Ticker.addEventListener("tick", handleTick);
-    createjs.Ticker.setFPS(60);
+    createjs.Ticker.setFPS(120);
+
 
     this.document.onkeydown = keydown;
     this.document.onkeyup = keyup;
@@ -32,13 +37,26 @@ function init() {
         delete keys[event.keyCode];
     }
 
-    function handleTick() {
-        if (keys[68]) playerSquare.velocity -= playerAccel;
-        if (keys[70]) playerSquare.velocity += playerAccel;
-        if (playerSquare.velocity > 0) playerSquare.velocity -= playerDecel;
-        else if (playerSquare.velocity < 0) playerSquare.velocity += playerDecel;
-        if (Math.abs(playerSquare.velocity) < playerDecel) playerSquare.velocity = 0;
-        playerSquare.x += playerSquare.velocity;
+
+    function movePlayerBox(box, acceleration, time) {
+
+        box.x = (.5 * acceleration * Math.pow(time,2)) + box.velocity * time + box.x;
+        box.velocity = acceleration * time + box.velocity;
+
+    }
+
+    function handleTick(event) {
+
+        acceleration = 0;
+
+        //calculate acceleration of player based on pressed keys
+        if (keys[68]) acceleration -= playerAccel;
+        if (keys[70]) acceleration += playerAccel;
+
+        if (playerSquare.velocity > 0) acceleration -= playerDecel;
+        else if (playerSquare.velocity < 0) acceleration += playerDecel;
+
+        movePlayerBox(playerSquare, acceleration, event.delta/1000);
         stage.update();
     }
 }
