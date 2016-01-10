24ea7326b30aef76cd8039749d1e19b06c545f1f
Quinton Woodmansee <qwoodmansee@capital.edu>
2016-01-10 00:06:12 -0500

    Changed circle to a square and set the color to be correct as per specification.

--- src/game.js
+++ src/game.js
@@ -9,16 +9,12 @@ playerDecel = .1;
 function init() {
     //Create a stage by getting a reference to the canvas
     stage = new createjs.Stage("demoCanvas");
-    //Create a Shape DisplayObject.
-    circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(210, 600, 40);
-    circle.velocity = 0;
 
-    //Set position of Shape instance.
-    circle.x = circle.y = 50;
-    //Add Shape instance to stage display list.
-    stage.addChild(circle);
-    //Update stage will render next frame
+    //create a square object
+    playerSquare = new createjs.Shape();
+    playerSquare.graphics.beginFill("#1643A3").drawRect(220,585,90,90);
+    playerSquare.velocity = 0;
+    stage.addChild(playerSquare);
     stage.update();
 
     //Update stage will render next frame
@@ -37,12 +33,12 @@ function init() {
     }
 
     function handleTick() {
-        if (keys[68]) circle.velocity -= playerAccel;
-        if (keys[70]) circle.velocity += playerAccel;
-        if (circle.velocity > 0) circle.velocity -= playerDecel;
-        else if (circle.velocity < 0) circle.velocity += playerDecel;
-        if (Math.abs(circle.velocity) < playerDecel) circle.velocity = 0;
-        circle.x += circle.velocity;
+        if (keys[68]) playerSquare.velocity -= playerAccel;
+        if (keys[70]) playerSquare.velocity += playerAccel;
+        if (playerSquare.velocity > 0) playerSquare.velocity -= playerDecel;
+        else if (playerSquare.velocity < 0) playerSquare.velocity += playerDecel;
+        if (Math.abs(playerSquare.velocity) < playerDecel) playerSquare.velocity = 0;
+        playerSquare.x += playerSquare.velocity;
         stage.update();
     }
 }
